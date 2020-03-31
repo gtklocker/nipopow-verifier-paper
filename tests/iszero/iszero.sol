f28c5e59a9f5bd421c5eed22b6a6d85b122893c8
sdaveas <sdaveas@gmail.com>
2020-03-31 19:12:02 +0300

    Add both function ins contract

--- tests/iszero/iszero.sol
+++ tests/iszero/iszero.sol
@@ -1,9 +1,15 @@
 pragma solidity ^0.6.0;
 
-contract test {
-    function func(uint256 input) public pure returns(bool){
-        // if (input%2 == 1) {
+contract testComp {
+    function compWith0(uint256 input) public pure returns(bool){
         if (!(input%2 == 0)) {
+            return true;
+        }
+        return false;
+    }
+
+    function compWith1(uint256 input) public pure returns(bool){
+        if (input%2 == 1) {
             return true;
         }
         return false;
